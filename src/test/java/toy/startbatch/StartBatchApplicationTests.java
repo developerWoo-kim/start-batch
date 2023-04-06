5629ead78fd3833b04116430c35cfcefc5dd49ba
gimgeon-u <gunwo151@gmail.com>
2023-04-06 19:19:42 +0900

    스프링 배치 프로젝트 첫 배포

---
+++ src/test/java/toy/startbatch/StartBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.startbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
